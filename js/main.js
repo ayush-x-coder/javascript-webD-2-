069c6355dbcbdec5dd2a1febc4b89aee82cabdc4
Ayush Gangwar <ayush.gangwar.2k204113@gmail.com>
2026-06-18 23:22:13 +0530

    sum 1 to 10

--- js/main.js
+++ js/main.js
@@ -24,6 +24,11 @@ console.log(value >= 0 ? "Positive" : "Negative");
 for (let i = 1; i <= 10; i++) {
   console.log(i);
 }
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+console.log(sum);
 
 
 
